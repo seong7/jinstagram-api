6a7bdc51860a7a8df0ba549590d569db7514b8a6
Seongjin <seongjinkim1123@gmail.com>
2020-06-22 12:11:41 +0000

    Create auth/auth.ctrl.js

---
+++ src/api/auth/auth.ctrl.js
@@ -0,0 +1,28 @@
+
+/* 
+  POST /api/auth/register
+  {
+    username: 'seongjin',
+    password: '123'
+  }
+*/
+export const register = ctx => {}
+
+/*
+  POST /api/auth/login
+  {
+    username: 'seongjin',
+    password: '123'
+  }
+*/
+export const login = ctx => {}
+
+/* 
+  GET /api/auth/check
+*/
+export const check  = async ctx => {}
+
+/* 
+  POST /api/auth/logout
+*/
+export const logout = async ctx => {}
