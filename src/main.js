0763b095c37c1a44b95ad0dd2e5bf2a667901319
Seongjin <seongjinkim1123@gmail.com>
2020-06-22 11:52:23 +0000

    Update main.js - Listen

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParder from 'koa-bodyparser';
 import mongoose from 'mongoose';
+
+const { PORT } = process.env;
+const app = new Koa();
+
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log("Listening to port %d", port);
+})
