5f6df5137c9d8eaae1974cec0aa839da18b284a3
Seongjin <seongjinkim1123@gmail.com>
2020-06-22 12:14:39 +0000

    Create src/models/user.js

---
+++ src/models/user.js
@@ -0,0 +1,13 @@
+import mogoose, { Schema } from 'mongoose';
+
+// schema 선언
+const UserSchema = new Schema( {
+  username: String,
+  hashedPassword: String,
+});
+
+// method 생성
+
+// Model 생성
+const User = mongoose.model('User', UserSchema);
+export default user;
