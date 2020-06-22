d4f5e229748d261883f709f84362bf9aa76c568c
Seongjin <seongjinkim1123@gmail.com>
2020-06-22 12:04:25 +0000

    Connect to MongoDB /sns from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
+require('dotenv').config();
 import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParder from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
-const { PORT } = process.env;
+const { PORT, MONGO_URI } = process.env;
 const app = new Koa();
+const router = new Router();
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connect to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const port = PORT || 4000;
 app.listen(port, () => {
-  console.log("Listening to port %d", port);
-})
+  console.log('Listening to port %d', port);
+});
