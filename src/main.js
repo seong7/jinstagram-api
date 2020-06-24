570dc500401d69dd71345c356e76fc0c6e3e3ca8
Seongjin <seongjinkim1123@gmail.com>
2020-06-24 12:34:07 +0000

    Add @koa/cors - allow CORS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import Koa from 'koa';
+import cors from '@koa/cors';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
@@ -12,10 +13,12 @@ const router = new Router();
 
 // 라우터 설정
 router.use('/api', api.routes());
-  
+
 // 라우터 적용 전에 미들웨어 적용 (서버에 요청 시 무조건 실행됨)
+app.use(cors());
 app.use(bodyParser());
 
+// 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 // mongoose 연결
